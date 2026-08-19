0e78c3d73a57fd1c8f248c506f451eb35f7b31fe
uyrerrf <unitedstategood@gmail.com>
2026-08-19 03:00:21 +0100

    Update XrcForegroundService.kt

--- android/app/src/main/java/com/xrc/svc/XrcForegroundService.kt
+++ android/app/src/main/java/com/xrc/svc/XrcForegroundService.kt
@@ -12,7 +12,6 @@ import android.os.Build
 import android.os.IBinder
 import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
-import com.xrc.R
 
 class XrcForegroundService : Service() {
 
